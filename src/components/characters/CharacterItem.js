ca00f16d1e4c66047405c6e13fd79829f469b531
Danielle Moss <dani.code.camp@gmail.com>
2020-07-28 16:04:20 -0400

    added character name to image alt tag

--- src/components/characters/CharacterItem.js
+++ src/components/characters/CharacterItem.js
@@ -5,7 +5,7 @@ const CharacterItem = ({ item }) => {
     <div className="card">
       <div className="card-inner">
         <div className="card-front">
-          <img src={item.img} alt="" />
+          <img src={item.img} alt={item.name} />
         </div>
         <div className="card-back">
           <h1>{item.name}</h1>
